dc9c663e87ee8d92cf5a12bee8281ad252db5d98
Philipp Krüger <philipp.krueger1@gmail.com>
2025-02-26 10:00:07 +0100

    Switch from "power-profiles-daemon" to "tlp". Best decision ever.

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -90,6 +90,20 @@
     xkbVariant = "";
   };
 
+  # try tlp over ppd:
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    # settings = {
+    #   CPU_BOOST_ON_AC = 1;
+    #   CPU_BOOST_ON_BAT = 0;
+    #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    # };
+  };
+  # thermald for better performance?
+  services.thermald.enable = true;
+
   # Configure console keymap
   console.keyMap = "de";
 
