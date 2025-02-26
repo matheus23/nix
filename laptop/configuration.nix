e5c4fc78f7c6475f23a5fa4f8710c20437670b26
Philipp Krüger <philipp.krueger1@gmail.com>
2025-02-26 10:22:35 +0100

    Play around with settings a bit

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -94,12 +94,13 @@
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
-    # settings = {
-    #   CPU_BOOST_ON_AC = 1;
-    #   CPU_BOOST_ON_BAT = 0;
-    #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    # };
+    settings = {
+      RESTORE_THRESHOLDS_ON_BAT = 1;
+      # CPU_BOOST_ON_AC = 1;
+      # CPU_BOOST_ON_BAT = 0;
+      # CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
   };
   # thermald for better performance?
   services.thermald.enable = true;
