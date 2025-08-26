71b6f20309132cc9ed2feb629ae3d897c4129ddf
Philipp Krüger <philipp.krueger1@gmail.com>
2025-08-26 12:32:03 +0200

    Add flake for running netsim/chuck

--- flake.nix
+++ flake.nix
@@ -200,5 +200,60 @@
             menu
           '';
         };
+
+      # For running netsim/chuck locally
+      devShells.netsim =
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          ovsScripts = "${pkgs.openvswitch}/share/openvswitch/scripts";
+
+          pyPkgs = with pkgs.python3Packages; [
+            pyshark
+            drawsvg
+            dpkt
+            humanfriendly
+            mininet-python
+          ];
+
+          netsim = pkgs.writeScriptBin "netsim" ''
+            #!/bin/sh
+
+            echo "Running with PYTHONPATH: $PYTHONPATH"
+            sudo PYTHONPATH=$PYTHONPATH python3 main.py $@
+          '';
+
+        in
+        pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              inetutils
+              mininet
+              openvswitch
+              iperf
+              tshark
+              python3
+              netsim
+            ]
+            ++ pyPkgs;
+
+          shellHook = ''
+            export OVS_DBDIR=$(pwd)
+            sudo ${ovsScripts}/ovs-ctl start \
+              --db-file="$OVS_DBDIR/conf.db" \
+              --system-id=random
+
+            sudo ovs-vsctl show
+
+            cleanup() {
+              sudo ${ovsScripts}/ovs-ctl stop
+              sudo rm $OVS_DBDIR/conf.db
+            }
+            trap cleanup EXIT
+          '';
+        };
+
     });
+
 }
