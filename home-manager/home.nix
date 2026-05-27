60a01bca55a8e877f0d4fb565a53746d4424240b
Philipp Krüger <philipp.krueger1@gmail.com>
2026-05-27 16:24:52 +0200

    Add framework desktop config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,107 +4,107 @@ let
   unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
 
   # Virtual Studio Code config
-  vscode = pkgs.vscode-with-extensions.override {
-    vscodeExtensions =
-      with pkgs.vscode-extensions;
-      [
-        # rust-lang.rust-analyzer
-        brettm12345.nixfmt-vscode
-        # ms-vsliveshare.vsliveshare
-        eamodio.gitlens
-        elmtooling.elm-ls-vscode
-        vadimcn.vscode-lldb
-        denoland.vscode-deno
-        svelte.svelte-vscode
-        mkhl.direnv
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        # https://github.com/NixOS/nixpkgs/blob/42d815d1026e57f7e6f178de5a280c14f7aba1a5/pkgs/misc/vscode-extensions/update_installed_exts.sh
-        {
-          name = "rust-analyzer";
-          publisher = "rust-lang";
-          version = "0.4.2710";
-          hash = "sha256-H3d1OiDW4mVIGowlWB+ShjJ+zbaWCtWFRzc41o8YYU8=";
-        }
-        {
-          name = "theme-atom-one-light";
-          publisher = "b4456609";
-          version = "0.2.4";
-          sha256 = "08a7v7i9vap15ygahfh9gcmr0m1mzikh3w3h8ajan6skadb6zfpf";
-        }
-        {
-          name = "Nix";
-          publisher = "bbenoist";
-          version = "1.0.1";
-          sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
-        }
-        {
-          name = "elm-ls-vscode";
-          publisher = "elmTooling";
-          version = "2.8.0";
-          sha256 = "1yy1xya1jzcah6fg5p729z97ca6777x42b68iv4l4wyqv204fnzk";
-        }
-        {
-          name = "tldraw-vscode";
-          publisher = "tldraw-org";
-          version = "2.0.11";
-          sha256 = "0649kigssry5vvjmdf06g3fr2bsnjp0a7sb3dvazi9qwp00c80c9";
-        }
-        {
-          name = "even-better-toml";
-          publisher = "tamasfe";
-          version = "0.19.0";
-          sha256 = "0xfnprgbafy7sfdqwdw92lr8k3h3fbylvhq1swgv31akndm9191j";
-        }
-        {
-          name = "wati";
-          publisher = "NateLevin";
-          version = "1.0.3";
-          sha256 = "0halx02zjgsara63qqyrgnjc1w9mxcb8ir4ywsvwgcnk7kg1iczv";
-        }
-        {
-          name = "vscode-wasm";
-          publisher = "dtsvet";
-          version = "1.4.0";
-          sha256 = "0p3a8brwpbg3fkhpq257jp7dnydk5b89ramb5yqpdp4yaksvfry5";
-        }
-        {
-          # required for ElmLS to work
-          name = "vscode-test-explorer";
-          publisher = "hbenl";
-          version = "2.21.1";
-          sha256 = "022lnkq278ic0h9ggpqcwb3x3ivpcqjimhgirixznq0zvwyrwz3w";
-        }
-        {
-          # Dependency of vscode-test-explorer above
-          name = "test-adapter-converter";
-          publisher = "ms-vscode";
-          version = "0.1.6";
-          sha256 = "0pj4ln8g8dzri766h9grdvhknz2mdzwv0lmzkpy7l9w9xx8jsbsh";
-        }
-        {
-          # Tailwind intellisense ... for deno (fresh)?
-          name = "twind-intellisense";
-          publisher = "sastan";
-          version = "0.2.1";
-          sha256 = "1lp7i2fw9ycr6x7rfw7zcr81pch250xw0pdg19xn3ic8wpdwdspp";
-        }
-        {
-          # zig language server & more
-          name = "vscode-zig";
-          publisher = "ziglang";
-          version = "0.5.1";
-          sha256 = "1m25bbgfv8x8f0ywadjwsmh4myqgp8xwf5yjrkskgr8axj8ny36a";
-        }
-        {
-          # frontend coding AI thingie
-          name = "kombai";
-          publisher = "kombai";
-          version = "1.4.272";
-          hash = "sha256-ZiqB4BtHLT46y1hICyxi+pAQu7Cw712gzt93LsBocNw=";
-        }
-      ];
-  };
+  # vscode = pkgs.vscode-with-extensions.override {
+  #   vscodeExtensions =
+  #     with pkgs.vscode-extensions;
+  #     [
+  #       # rust-lang.rust-analyzer
+  #       brettm12345.nixfmt-vscode
+  #       # ms-vsliveshare.vsliveshare
+  #       eamodio.gitlens
+  #       elmtooling.elm-ls-vscode
+  #       vadimcn.vscode-lldb
+  #       denoland.vscode-deno
+  #       svelte.svelte-vscode
+  #       mkhl.direnv
+  #     ]
+  #     ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+  #       # https://github.com/NixOS/nixpkgs/blob/42d815d1026e57f7e6f178de5a280c14f7aba1a5/pkgs/misc/vscode-extensions/update_installed_exts.sh
+  #       {
+  #         name = "rust-analyzer";
+  #         publisher = "rust-lang";
+  #         version = "0.4.2710";
+  #         hash = "sha256-H3d1OiDW4mVIGowlWB+ShjJ+zbaWCtWFRzc41o8YYU8=";
+  #       }
+  #       {
+  #         name = "theme-atom-one-light";
+  #         publisher = "b4456609";
+  #         version = "0.2.4";
+  #         sha256 = "08a7v7i9vap15ygahfh9gcmr0m1mzikh3w3h8ajan6skadb6zfpf";
+  #       }
+  #       {
+  #         name = "Nix";
+  #         publisher = "bbenoist";
+  #         version = "1.0.1";
+  #         sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  #       }
+  #       {
+  #         name = "elm-ls-vscode";
+  #         publisher = "elmTooling";
+  #         version = "2.8.0";
+  #         sha256 = "1yy1xya1jzcah6fg5p729z97ca6777x42b68iv4l4wyqv204fnzk";
+  #       }
+  #       {
+  #         name = "tldraw-vscode";
+  #         publisher = "tldraw-org";
+  #         version = "2.0.11";
+  #         sha256 = "0649kigssry5vvjmdf06g3fr2bsnjp0a7sb3dvazi9qwp00c80c9";
+  #       }
+  #       {
+  #         name = "even-better-toml";
+  #         publisher = "tamasfe";
+  #         version = "0.19.0";
+  #         sha256 = "0xfnprgbafy7sfdqwdw92lr8k3h3fbylvhq1swgv31akndm9191j";
+  #       }
+  #       {
+  #         name = "wati";
+  #         publisher = "NateLevin";
+  #         version = "1.0.3";
+  #         sha256 = "0halx02zjgsara63qqyrgnjc1w9mxcb8ir4ywsvwgcnk7kg1iczv";
+  #       }
+  #       {
+  #         name = "vscode-wasm";
+  #         publisher = "dtsvet";
+  #         version = "1.4.0";
+  #         sha256 = "0p3a8brwpbg3fkhpq257jp7dnydk5b89ramb5yqpdp4yaksvfry5";
+  #       }
+  #       {
+  #         # required for ElmLS to work
+  #         name = "vscode-test-explorer";
+  #         publisher = "hbenl";
+  #         version = "2.21.1";
+  #         sha256 = "022lnkq278ic0h9ggpqcwb3x3ivpcqjimhgirixznq0zvwyrwz3w";
+  #       }
+  #       {
+  #         # Dependency of vscode-test-explorer above
+  #         name = "test-adapter-converter";
+  #         publisher = "ms-vscode";
+  #         version = "0.1.6";
+  #         sha256 = "0pj4ln8g8dzri766h9grdvhknz2mdzwv0lmzkpy7l9w9xx8jsbsh";
+  #       }
+  #       {
+  #         # Tailwind intellisense ... for deno (fresh)?
+  #         name = "twind-intellisense";
+  #         publisher = "sastan";
+  #         version = "0.2.1";
+  #         sha256 = "1lp7i2fw9ycr6x7rfw7zcr81pch250xw0pdg19xn3ic8wpdwdspp";
+  #       }
+  #       {
+  #         # zig language server & more
+  #         name = "vscode-zig";
+  #         publisher = "ziglang";
+  #         version = "0.5.1";
+  #         sha256 = "1m25bbgfv8x8f0ywadjwsmh4myqgp8xwf5yjrkskgr8axj8ny36a";
+  #       }
+  #       {
+  #         # frontend coding AI thingie
+  #         name = "kombai";
+  #         publisher = "kombai";
+  #         version = "1.4.272";
+  #         hash = "sha256-ZiqB4BtHLT46y1hICyxi+pAQu7Cw712gzt93LsBocNw=";
+  #       }
+  #     ];
+  # };
 
   signingPubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4oFL+qMOmADJ+KGZwQ13Ma65zcEcXuF4JYjNrjvIr5 nixos git commit signing for philipp.krueger1@gmail.com";
 
@@ -145,7 +145,7 @@ in
   programs.home-manager.enable = true;
 
   # VSCode settings "sync"
-  home.file.".config/Code/User/settings.json".text = builtins.readFile ./vscode/settings.json;
+  # home.file.".config/Code/User/settings.json".text = builtins.readFile ./vscode/settings.json;
 
   # Git stuff
   programs.git = {
@@ -202,7 +202,7 @@ in
   # };
 
   home.packages = [
-    vscode
+    # vscode
     _1password
     (pkgs.spotify.override { deviceScaleFactor = 2; })
     pkgs.discord
