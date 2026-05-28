a3d4d5e2647701cf1aa5ad03cd5061b661cb349b
Philipp Krüger <philipp.krueger1@gmail.com>
2026-05-28 10:06:10 +0200

    Steam on desktop

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -108,10 +108,16 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     snapper-gui
   ];
+
+  # needed for steam?
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # needed for Steam (32-bit games)
+  };
+
+  programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
