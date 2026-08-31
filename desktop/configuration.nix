0bf81d473a4204ed92b55fe5330402d22d84724c
Philipp Krüger <philipp.krueger1@gmail.com>
2026-08-31 07:14:21 +0200

    Rename to llama-server

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -15,7 +15,7 @@ in
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./llama-qwen38.nix
+    ./llama-server.nix
     <home-manager/nixos>
   ];
 
